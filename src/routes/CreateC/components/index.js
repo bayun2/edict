8bdacc7fae48c1a85bbc602311c6987e35e6ae98
rwt <rwt@rwtdeMBP.lan>
2017-01-23 00:03:33 +0800

    生成指令

--- src/routes/CreateC/components/index.js
+++ src/routes/CreateC/components/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import * as ReactRouter from 'react-router';
-import UrlParse from 'url-parse';
-
-const {Link} = ReactRouter;
+import fetch from 'isomorphic-fetch';
+const {Link, browserHistory} = ReactRouter;
 
 class CreateC extends React.Component {
   constructor(props) {
     super(props);
 
-    this.funcName = [];
+    this.funcName = ['replyEdict', 'handleChange'];
     this.funcName.forEach(funcName => {
       this[funcName] = this[funcName].bind(this);
     });
+  }
+
+  handleChange(event) {
+    let msg = event.target.value;
+    msg = msg.substr(0, 50);
+    this.props.setState({msg});
+  }
+
+  sendEdict() {
+    const self = this;
+    fetch(`${window.apiPath}/add${window.apiSuffix}`, {
+      credentials: 'include',
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/x-www-form-urlencoded;charset=UTF-8'
+      },
+      body: `msg=${self.props.msg}`
+    })
+      .then(response => response.json())
+      .then(json => {
+        self.props.setState({
+          image: json.image,
+          shareUrl: json.shareUrl
+        }, () => {
+          const url = this.props.addParam('created');
+          browserHistory.push(url);
+        });
+      })
+      .catch(() => {
+
+      });
   }
 
   render() {
@@ -19,10 +49,10 @@ class CreateC extends React.Component {
         <div className="createc page">
           <div className="title">
             <div className="cnt">
-              <textarea placeholder="点击起草圣旨"></textarea>
+              <textarea onChange={this.handleChange} placeholder="点击起草圣旨"></textarea>
             </div>
           </div>
-          <Link className="btn" to="/created">生成指令</Link>
+          <div className="btn" onClick={this.sendEdict}>生成指令</div>
         </div>
     );
   }
@@ -33,7 +63,8 @@ CreateC.defaultProps = {
 };
 
 CreateC.propTypes = {
-  goto: React.PropTypes.func
+  addParam: React.PropTypes.func,
+  setState: React.PropTypes.func
 };
 
 module.exports = CreateC;
